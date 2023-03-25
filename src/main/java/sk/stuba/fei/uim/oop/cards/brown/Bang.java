ee7293dabac2444ecc63af41ebfd8e60a5655af0
Myroslava Shevska <miroslavashevskaya12@gmail.com>
2023-03-25 19:22:47 +0100

    Corrected version of the Barrel

--- src/main/java/sk/stuba/fei/uim/oop/cards/brown/Bang.java
+++ src/main/java/sk/stuba/fei/uim/oop/cards/brown/Bang.java
@@ -5,8 +5,6 @@ import sk.stuba.fei.uim.oop.cards.Card;
 import sk.stuba.fei.uim.oop.cards.blue.Barrel;
 import sk.stuba.fei.uim.oop.player.Player;
 
-import java.util.stream.Stream;
-
 public class Bang extends Card {
     private static final String CARD_NAME = "Bang";
 
@@ -16,36 +14,26 @@ public class Bang extends Card {
 
     @Override
     public void playCard(Player player) {
+        boolean check = false;
         super.playCard(player);
         int playNumber = this.getPlayerNumber(player);
         Player targetPlayer = this.board.getPlayers()[playNumber];
 
 
-        Card barrel = targetPlayer.getCards().stream()
-                .filter(card -> card instanceof Barrel)
-                .findAny()
-                .orElse(null);
+        Card barrel = targetPlayer.getBlueCards().stream()
+                .filter(card -> card instanceof Barrel).findAny().orElse(null);
 
-        Card missedCard = targetPlayer.getCards().stream().filter(card -> card instanceof Missed).findFirst().orElse(null);
+        Card missedCard = targetPlayer.getCards().stream()
+                .filter(card -> card instanceof Missed).findFirst().orElse(null);
 
         if (barrel != null) {
-            if (!((Barrel) barrel).checkEffect(player)) {
-                // если checkEffect(player) вернула false, то продолжаем выполнение в блоке else if
-                System.out.println("Barrel effect failed!");
-
-                if (missedCard != null) {
-                    missedCard.playCard(targetPlayer);
-                    this.board.addGameCard(this);
-                    System.out.println("Missed после Barrel!");
-                } else {
-                    targetPlayer.removeLife();
-                    System.out.println("Player " + targetPlayer.getName() + "was not protected by a Barrel card and the player did not have a Missed card!");
-                    System.out.println("Player " + targetPlayer.getName() + " lost his/her live.. -💔x1" + "  ❤x" + targetPlayer.getLives());
-                }
-            } else {
-                System.out.println("The Barrel card kept the player alive!");
+            check = ((Barrel) barrel).checkEffect(player);
+            if (check) {
+                System.out.println("The player was protected from the Bang card by the Barrel card! 💗🎴");
             }
-        } else if (missedCard != null) {
+        }
+
+        if (!check && missedCard != null) {
             missedCard.playCard(targetPlayer);
             this.board.addGameCard(this);
         } else {
